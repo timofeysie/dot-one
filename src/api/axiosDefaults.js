cc924f50b168b39ce1e626ac6e354a06f55ceeb6
tim_curchod <timofeyc@hotmail.com>
2024-12-30 09:46:07 +0900

    add nest deployed ec2 instance url for real time trend use

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,11 +4,12 @@ axios.defaults.baseURL = "https://drf-two-eb17ecbff99f.herokuapp.com/";
 // axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
 // axios.defaults.baseURL = "https://unified-moments.herokuapp.com/";
 // axios.defaults.baseURL = "http://127.0.0.1:8000/"; // running locally
+// axios.defaults.baseURL = "http://localhost:3001"; // running Nest backend locally
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosNest = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "http://ec2-52-65-222-223.ap-southeast-2.compute.amazonaws.com:3001",
   headers: {
     "Content-Type": "application/json",
   },
